daac9adc8c1dc23f5af22f2611eb3cc860a5f9a3
githong <githong@kakao.com>
2021-01-03 23:55:43 +0900

    Modify Main Method's Structure For Readability

--- src/com/bale/kabang/SWJavaProgramming.java
+++ src/com/bale/kabang/SWJavaProgramming.java
@@ -11,16 +11,3 @@ public class SWJavaProgramming {
         Ch23.run();
     }
 }
-
-class Ch23 {
-    public static void run() {
-        // 배열 기반 리스트 컬렉션 예제
-        //ArrayListCollection.main();
-
-        // 링크드리스트 기반 리스트 컬렉션 예제
-        //LinkedListCollection.main();
-
-        // 반복자를 통한 리스트 순차 조회 예제
-        IteratorCollection.main();
-    }
-}
