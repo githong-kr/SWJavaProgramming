20d8c3eacf3f330923cc218a6c3592d2fac03498
githong@kakao.com <rkfaorl12!@>
2021-04-25 23:49:52 +0900

    Chapter 26 added

---
+++ src/com/bale/kabang/ch26/Ch26.java
@@ -0,0 +1,11 @@
+package com.bale.kabang.ch26;
+
+public class Ch26 {
+    public static void run() {
+        // 정렬 기준을 인터페이스를 구현한 클래스를 두어서 구현하는 예제
+        //SortComparator.main();
+
+        // 정렬 기준을 익명클래스를 사용해 구현하는 예제
+        AnonymousComparator.main();
+    }
+}
---
+++ src/com/bale/kabang/ch26/SortComparator.java
@@ -0,0 +1,26 @@
+package com.bale.kabang.ch26;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class StrComp implements Comparator<String> {
+    @Override
+    public int compare(String s1, String s2) {
+        return s1.length() - s2.length();
+    }
+}
+
+public class SortComparator {
+    public static void main() {
+        List<String> list = new ArrayList<>();
+        list.add("ROBOT");
+        list.add("APPLE");
+        list.add("BOX");
+
+        StrComp cmp = new StrComp();
+        Collections.sort(list, cmp);
+        System.out.println(list);
+    }
+}
